cca59d7ab354a310e5c9993bc562c0195cde0688
SelimHorri <horri.slim@gmail.com>
2021-04-11 17:33:19 +0100

    init

---
+++ src/main/java/com/selimhorri/pack/UploadDownloadFilesOverHttpApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadFilesOverHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadFilesOverHttpApplication.class, args);
+	}
+
+}
