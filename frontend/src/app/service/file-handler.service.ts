e65c9c1b24d29fd9ca6d5ee4460487e9918330a4
SelimHorri <horri.slim@gmail.com>
2021-04-25 15:10:14 +0100

    implement file handler service upload and download

--- frontend/src/app/service/file-handler.service.ts
+++ frontend/src/app/service/file-handler.service.ts
@@ -1,9 +1,40 @@
+
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileHandlerService {
-
-  constructor() { }
+  
+  private apiUrl: string = environment.apiUrl;
+  
+  constructor(private http: HttpClient) {
+    
+  }
+  
+  public upload(formData: FormData): Observable<HttpEvent<string[]>> {
+    return this.http.post<string[]>(this.apiUrl + "/files/upload", formData, {
+      reportProgress: true,
+      observe: "events"
+    });
+  }
+  
+  public download(filename: string): Observable<HttpEvent<Blob>> {
+    return this.http.get(this.apiUrl + "/files/download" + "/" + filename, {
+      reportProgress: true,
+      observe: "events",
+      responseType: "blob"
+    });
+  }
+  
+  
+  
 }
+
+
+
+
+
