e74d4daaf8f82cd71706233af917144ed79c3fd2
SelimHorri <horri.slim@gmail.com>
2021-04-25 16:35:52 +0100

    setup method configs

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -19,12 +19,13 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
+  /*
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontend');
   });
+  */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- frontend/src/app/component/home/home.component.ts
+++ frontend/src/app/component/home/home.component.ts
@@ -1,4 +1,8 @@
+
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FileHandlerService } from 'src/app/service/file-handler.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  public filenames: string[] = [];
+  public fileStatus = {
+    status: "",
+    requestType: "",
+    percent: 0
+  };
+  
+  constructor(private fileHandlerService: FileHandlerService) {
+    
   }
-
+  
+  ngOnInit(): void {
+    
+  }
+  
+  public onUploadFiles(files: File[]): void {
+    
+    const formData: FormData = new FormData();
+    files.forEach(f => formData.append("files", f, f.name));
+    this.fileHandlerService.upload(formData).subscribe(
+      (response) => {
+        console.log(response);
+        this.reportProgress(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+        alert(error.message);
+      }
+    );
+  }
+  
+  private reportProgress(httpEvent: HttpEvent<string[] | Blob>): void {
+    switch (httpEvent.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, "Uploading...");
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, "Downloading...");
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log("header returned...", httpEvent);
+        break;
+      case HttpEventType.Response:
+        if (httpEvent.body instanceof Array) {
+          httpEvent.body.forEach(filename => this.filenames.unshift(filename));
+        }
+        else {
+          saveAs(new File([httpEvent.body!], httpEvent.headers.get('File-Name')!, { type: httpEvent.headers.get('Content-Type') + ";charset=utf-8"} ));
+        }
+        break;
+        default:
+          console.log(httpEvent);
+    }
+  }
+  
+  private updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = "progress";
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round(100 * loaded / total);
+  }
+  
+  public onDownloadFiles(filename: string): void {
+    
+    this.fileHandlerService.download(filename).subscribe(
+      (response) => {
+        console.log(response);
+        this.reportProgress(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+        alert(error.message);
+      }
+    );
+  }
+  
+  
+  
 }
